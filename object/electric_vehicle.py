ac31675352030d720f4953b014ed0f0a01463f7e
iskandarmrp <iskandarmudarp@gmail.com>
2025-05-19 00:40:54 +0700

    simulasi

--- object/electric_vehicle.py
+++ object/electric_vehicle.py
@@ -1,3 +1,5 @@
+from algorithm.utils import calculate_energy_model
+
 class ElectricVehicle:
     def __init__(self, type, max_speed_kmh, battery_capacity, battery_now, charging_rate, current_lat, current_lon):
         self.type = type
@@ -7,3 +9,28 @@ class ElectricVehicle:
         self.charging_rate = charging_rate  # Kecepatan Ngecas (kWh per menit)
         self.current_lat = current_lat # Latitude Sekarang
         self.current_lon = current_lon # Longitude Sekarang
+
+    def drive(self, env, G, charging_at, charging_stations, from_node, to_node, distance_km, duration, duration_in_hour, speed):
+        # Cek apakah kecepatannya tidak melebihi kecepatan maksimal pengendara
+        if speed > self.max_speed:
+            speed = self.max_speed
+
+        energy_consumed = calculate_energy_model(distance_km, speed, self.type)
+
+        # Menghitung waktu
+        travel_time = (distance_km / speed) * 60  # Waktu Travel (Ubah ke dalam menit)
+
+        # Melakukan cas jika terdapat di schedule
+        if (G.nodes[from_node].get("is_charging_station") and ((from_node in charging_at) or (self.battery_now < 0.2 * self.capacity))):
+            if (from_node in charging_at):
+                charging_duration = charging_at[from_node]['charging_time']
+                charging_rate = charging_at[from_node]['charging_rate']
+            else:
+                charging_duration = 20
+                charging_rate = 999
+            yield from charging_stations[from_node].charge(self, charging_duration, charging_rate)
+
+        print(f"[{env.now:.2f}m] {self.type} mulai dari {from_node} ke {to_node} (jarak: {distance_km} km)")
+        yield env.timeout(travel_time)
+        self.battery_now -= energy_consumed
+        print(f"[{env.now:.2f}m] {self.type} sampai di {to_node}, baterai: {self.battery_now:.2f} kWh")
---
+++ simulasi.py
@@ -0,0 +1,378 @@
+# import
+import simpy
+import osmnx as ox
+import numpy as np
+import pickle
+
+from object.charging_station import (
+    ChargingSlot,
+    ChargingStation
+)
+from object.electric_vehicle import (
+    ElectricVehicle
+)
+from algorithm.algorithm import (hybrid_pso_alns_evrp)
+
+
+# Function dan Procedure
+
+# function user_input
+def user_input(vehicle_type):
+    """
+    function untuk user melakukan input data EV
+    param:
+    vehicle_type -> tipe kendaraan ('Tesla S', 'BMW i3', 'Nissan Leaf', or 'Other')
+    """
+    # Cek apakah input benar
+    if vehicle_type not in ['tesla s', 'bmw i3', 'nissan leaf', 'other']:
+        raise ValueError("Invalid vehicle type! Choose from 'Tesla S', 'BMW i3', 'Nissan Leaf', or 'Other'.")
+    
+    # Inisialisasi
+    battery_capacity = 100
+    charging_rate = 50
+
+    # Masukkan input sesuai tipe kendaraan
+    if vehicle_type == "tesla s":
+        vehicle_type = "tesla"
+        battery_capacity = 98
+        charging_rate = 125
+    elif vehicle_type == "bmw i3":
+        vehicle_type = "bmw"
+        battery_capacity = 37.9
+        charging_rate = 47
+    elif vehicle_type == "nissan leaf":
+        vehicle_type = "leaf"
+        battery_capacity = 39
+        charging_rate = 40
+    else:
+        vehicle_type = "other"
+        battery_capacity = float(input("Enter battery capacity in kWh: "))
+        charging_rate = float(input("Enter charging rate in kW: "))
+    
+    # Melakukan input lanjutan EV dari User
+    max_speed_kmh = float(input("Enter maximum speed in km/h: "))
+    battery_now = float(input("Enter current battery level in kWh: "))
+    current_lat = float(input("Enter current latitude: "))
+    current_lon = float(input("Enter current longitude: "))
+
+    # Menghasilkan dictionary untuk input EV
+    return {
+        'type': vehicle_type,
+        'max_speed_kmh': max_speed_kmh,
+        'battery_capacity': battery_capacity,
+        'battery_now': battery_now,
+        'charging_rate': charging_rate,
+        'current_lat': current_lat,
+        'current_lon': current_lon
+    }
+
+# todo: get_route (pakai algoritma yang dibuat)
+# function get_route
+def get_route(init_node, dest_node, G, ev):
+    """
+    function yang melakukan routing dari init_node ke dest_node
+
+    function ini merupakan pengganti route dari BE dan route dari myBlueBird(pokonya routing itu dari luar simulasi) -> simulasimah hanya nerima route dan mensimulasikannya
+
+    param: 
+    init_node -> id node awal
+    dest_node -> id node tujuan
+    G -> graph (G yang harus sudah ada kolom duration pada G.edges(keys=True, data=True))
+
+    return route yang berupa array of node_id
+    """
+    gbest_position, gbest_route, gbest_charge, gbest_cost, gbest_visit_decision, trace = hybrid_pso_alns_evrp(G, ev, init_node, dest_node, n_particles=10, max_iter=1000, stagnation=500, max_time=120)
+    # route = nx.shortest_path(G, init_node, dest_node, weight='duration')
+    return gbest_route, gbest_charge
+
+def get_node_by_latlon(G, lat, lon):
+    """
+    mencari node sesuai dengan latitude dan longitude
+
+    param:
+    G -> graph
+    lat -> latitude
+    lon -> longitude
+    """
+    for node, data in G.nodes(data=True):
+        if data.get('lat') == lat and data.get('lon') == lon:
+            return node
+    return None
+
+def get_nearest_node(G, lat, lon):
+    """
+    mengambil node terdekat dari lat dan lon tertentu
+
+    param:
+    G -> graph (G yang harus sudah ada kolom duration pada G.edges(keys=True, data=True))
+    lat -> latitude
+    lon -> longitude
+    """
+    return ox.distance.nearest_nodes(G, float(lon), float(lat))
+
+def get_node_lat_lon(G, node_id):
+    """
+    mengambil latitude dan longitude dari node_id
+
+    param:
+    G -> graph (G yang harus sudah ada kolom duration pada G.edges(keys=True, data=True))
+    node_id -> id dari node
+
+    return: 
+    (lat, lon)
+    """
+    numpy_nodes = np.array(list(G.nodes(data=True)))
+    mask = numpy_nodes[:, 0] == node_id
+    filtered = numpy_nodes[mask]
+    node = filtered[0]
+    lat, lon = float(node[1]["y"]), float(node[1]["x"])
+    return (lat, lon)
+
+# function get edges
+def find_edges(u, v, G):
+    """
+    menerima:
+    u -> id node asal
+    v -> id node tujuan
+    G -> graph (G yang harus sudah ada kolom duration pada G.edges(keys=True, data=True))
+
+    return: edges yang memiliki titik asal node dengan id u dan titik tujuan dengan node v
+    """
+    edges_numpy = np.array(list(G.edges(keys=True, data=True)))
+    mask = (edges_numpy[:, 0] == u) & (edges_numpy[:, 1] == v)
+    filtered = edges_numpy[mask]
+    return filtered[0]
+
+# procedure edges management (Weight: Duration) (Untuk graph awal)
+def manage_all_edges(G):
+    """
+    Melakukan sampling weight (Durasi) untuk Graph awal sebelum menentukan rute
+    param:
+    G -> graph
+
+    Contoh isi edge pada Graph: (0, 1, {'weight': 10.716666666666667, 'distance': 5.404, 'weight_mean': 716666666666667, 'weight_std': 1.0716666666666668})
+    """
+    for u, v, data in G.edges(data=True):
+        mean = data.get('weight_mean')
+        std = data.get('weight_std')
+
+        # Melakukan sampling weight berdasarkan 'weight_mean' dan 'weight_std'
+        sampled_weight = np.random.normal(loc=mean, scale=std)
+        sampled_weight = max(0, sampled_weight)
+
+        # Update weight (Duration)
+        data['weight'] = sampled_weight
+
+        print(f"[Manage Edges] Edge ({u} → {v}): {sampled_weight:.2f}")
+
+# procedure nodes management (Charging Station Availability) (Untuk graph awal)
+def manage_all_nodes(G):
+    """
+    Melakukan sampling availability untuk Graph awal sebelum menentukan rute
+    param:
+    G -> graph
+
+    Contoh isi Graph: (0, {'name': 'SPLU', 'lat': -6.920398, 'lon': 107.6088493, 'is_charging_station': True, 'total_slots': 2, 'slots_charging_rate': [1.0, 2.0], 'slots_availability_std': 0.5, 'slots_availability_mean': 1.55, 'slots_availability': [True, True]})
+    """
+    for station_id, data in G.nodes(data=True):
+        if data.get("is_charging_station", False):
+            total_slots = data.get("total_slots")
+            mean = data.get("slots_availability_mean")
+            std = data.get("slots_availability_std")
+
+            # Menghitung jumlah slot yang tersedia berdasarkan distribusi normal
+            available_count = int(np.random.normal(loc=mean, scale=std))
+            available_count = max(0, min(available_count, total_slots))  # clamp
+
+            # todo: Melakukan random tetapi dengan data distribusi slot yang sudah ada
+            # Randomize indeks slot yang akan available
+            available_indices = np.random.choice(range(total_slots), available_count, replace=False)
+
+            # Update slot availability: list boolean berdasarkan hasil random
+            slots_availability = [(i in available_indices) for i in range(total_slots)]
+            G.nodes[station_id]["slots_availability"] = slots_availability
+
+            print(f"[Manage Nodes] Station {station_id} → {available_count}/{total_slots} slot tersedia: {slots_availability}")
+
+# procedure edge management (Randomize Weight (Duration) untuk satu edge)
+def manage_edge(G, from_node, to_node):
+    """
+    Melakukan sampling weight (Durasi) untuk Graph awal sebelum menentukan rute
+    param:
+    G -> graph
+    from_node -> node awal
+    to_node -> node tujuan
+    """
+    if G.has_edge(from_node, to_node):
+        data = G.edges[from_node, to_node]
+
+        mean = data.get('weight_mean')
+        std = data.get('weight_std')
+
+        # Melakukan sampling weight berdasarkan 'weight_mean' dan 'weight_std'
+        sampled_weight = np.random.normal(loc=mean, scale=std)
+        sampled_weight = max(0, sampled_weight)
+
+        # Update weight (Duration)
+        data['weight'] = sampled_weight
+
+        print(f"[Manage Edge] Edge ({from_node} → {to_node}): {sampled_weight:.2f}")
+    else:
+        print(f"[Manage Edge] Edge ({from_node} → {to_node}) tidak ditemukan di graph.")
+
+# todo: buat simulasi rute (simulasiin rute yang didapat dari function get route)
+# procedure simulate route
+def simulate_route(env, G, charging_at, charging_stations, ev, route):
+    for i in range(len(route) - 1):
+        from_node = route[i]
+        to_node = route[i + 1]
+
+        # Ambil jarak antar node dari graph
+        distance_km = G.edges[from_node, to_node].get("distance", 1)  # Sudah dalam bentuk km
+        duration = G.edges[from_node, to_node].get("weight", 1)  # Durasi jalan dalam menit
+        duration_in_hour = duration / 60 # Ubah durasi ke dalam jam untuk menghitung kecepatan
+        speed = distance_km / duration_in_hour
+
+        # Simulasi drive dan charging
+        yield from ev.drive(env, G, charging_at, charging_stations, from_node, to_node, distance_km, duration, duration_in_hour, speed)
+
+# Object
+
+# SOKIN (BACA CLASS BENER GA)
+
+
+# todo: bikin class Simulation
+class Simulation:
+    def __init__(self, graph_file, ev_input, route, charging_at):
+        self.env = simpy.Environment() # Inisialisasi ENV
+        self.graph_file = graph_file # File Graph
+        self.G = None # Graph
+        self.ev_input = ev_input
+        self.ev = None # EV
+        self.charging_stations = {}
+        self.route = route # Rute
+        self.charging_at = charging_at # Schedule
+
+    # Melakukan load graph file
+    def load_graph(self):
+        with open(self.graph_file, "rb") as f:
+            self.G = pickle.load(f)
+
+    # Membuat banyak charging station menggunakan class charging station dengan csv
+    def setup_charging_stations(self):
+        for node_id, data in self.G.nodes(data=True):
+            if not data.get("is_charging_station", False):
+                continue  # Skip kalau bukan charging station
+
+            # Ambil informasi
+            slots_parameter = data.get("slots_parameter")
+            if not slots_parameter:
+                continue  # Skip kalau tidak ada data slot
+
+            # Kita ambil jumlah slot dari penjumlahan 'k' di setiap slot type
+            total_slots = sum(slot_info["s"] for slot_info in slots_parameter.values())
+
+            # Ambil charging rate per slot (e.g. [22, 22, ...] sebanyak total slot)
+            slots_charging_rate = []
+            for rate_str, slot_info in slots_parameter.items():
+                rate = int(rate_str.split()[0])  # ambil angka dari "22 kW"
+                slots_charging_rate.extend([rate] * slot_info["s"])
+
+            # Simpan ChargingStation
+            self.charging_stations[node_id] = ChargingStation(
+                env=self.env,
+                node_id=node_id,
+                name=data.get("name"),
+                latitude=data.get("latitude"),
+                longitude=data.get("longitude"),
+                total_slots=total_slots,
+                slots_charging_rate=slots_charging_rate,
+                slots_indicators=data.get("slots_indicator"),
+                slots_parameter=data.get("slots_parameter"),
+            )
+
+    # Membuat EV menggunakan class EV dengan input ev
+    def setup_electric_vehicle(self):
+        self.ev = ElectricVehicle(
+            **ev_input
+        )
+        print("EV berhasil dibuat")
+
+    def run(self):
+        global G
+        global charging_stations
+        global charging_at
+
+        self.load_graph()
+        self.setup_charging_stations()
+        self.setup_electric_vehicle()
+
+        G = self.G
+        charging_stations = self.charging_stations
+
+        charging_at = self.charging_at
+
+        self.env.process(simulate_route(self.env, self.G, self.charging_at, self.charging_stations, self.ev, self.route))
+        print('Simulasi sedang berjalan')
+        self.env.run()
+
+if __name__ == '__main__':
+    # ev_input = {
+    #     'type': 'bmw',
+    #     'max_speed_kmh': 60,
+    #     'battery_capacity': 50,
+    #     'battery_now': 20,
+    #     'charging_rate': 2.0,
+    #     'current_lat': -6.2,
+    #     'current_lon': 106.8
+    # }
+
+    ev_input = {
+        'type': 'tesla',
+        'max_speed_kmh': 100,
+        'battery_capacity': 130,
+        'battery_now': 90,
+        'charging_rate': 125,
+        'current_lat': -1.5882215,
+        'current_lon': 103.61938
+    }
+    
+    # vehicle_type = input("Enter vehicle type (Tesla S, BMW i3, Nissan Leaf, or Other): ").lower()
+    # ev_input = user_input(vehicle_type)
+
+    # Random charging station availability (Harusnya random buat Graph)
+    # manage_all_nodes(charging_stations)
+
+    # todo: Mendapatkan rute dari algoritma yang telah dibuat
+    # get_route()
+
+    # Sementara: Fixed Route, Nanti akan diganti dari algoritma
+    # route_nodes = [0, 1, 3] # Osmnx kayak gini
+    route_nodes = [34, 1, 152, 5, 80]
+
+    # charging_at = {
+    #     1: 10  # node 1 ngecas manual 10 menit
+    # }
+
+    charging_at = {
+        34: {'soc_start': 90, 'soc_target': 118, 'charging_rate': 22, 'charging_time': 76.36363636363636, 'waiting_time': 0.38},
+        1: {'soc_start': 12.78, 'soc_target': 93.75, 'charging_rate': 22, 'charging_time': 220.82, 'waiting_time': 0.38},
+        152: {'soc_start': 23.60, 'soc_target': 118, 'charging_rate': 30, 'charging_time': 188.80, 'waiting_time': 0.38},
+        5: {'soc_start': 14.86, 'soc_target': 82.67, 'charging_rate': 50, 'charging_time': 81.37, 'waiting_time': 0.38},
+    }
+    
+    # Inisialiasi simulasi dan menjalankan simulasi
+    # sim = Simulation(
+    #     graph_file="charging_station_graph.pkl",
+    #     ev_input = ev_input,
+    #     charging_stations_csv="charging_station.csv",
+    #     route=route_nodes,
+    #     charging_at = charging_at
+    # )
+    sim = Simulation(
+        graph_file="preprocessing_graph/spklu_sumatera_graph_with_parameters_231.pkl",
+        ev_input = ev_input,
+        route=route_nodes,
+        charging_at = charging_at
+    )
+    sim.run()
